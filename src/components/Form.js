362de13701fa46392e3b0efeaf9a8d5b9c959447
William Koury <wgkoury@gmail.com>
2020-05-18 16:56:50 -0400

    Cleaner layout

--- src/components/Form.js
+++ src/components/Form.js
@@ -59,111 +59,148 @@ class Input extends React.Component {
     }
 
     render(){
-
-        const Spacer = () => {
-            return (
-                <span> | Refine: </span>
-            );
-        };
-
         return(
-            <div className='form'>
+            <div className='content'>
+
+            <div className='row'>
                 <div className='column left'>
-                    <form>
-                        <p><label>What was your highest score on the math section of the SAT?</label></p>
-                        <input 
-                            type='range' 
-                            min={200} 
-                            max={800} 
-                            value={this.state.mathSAT} 
-                            step={10} name='mathSAT' 
-                            onChange={this.handleChange}/>
-                        <div>{this.state.mathSAT}</div>
-                        
-                        <p><label>What was your highest score on the verbal section of the SAT?</label></p>
-                        <input 
-                            type='range' 
-                            min={200} 
-                            max={800} 
-                            value={this.state.verbalSAT} 
-                            step={10} name='verbalSAT' 
-                            onChange={this.handleChange}/>
-                        <div>{this.state.verbalSAT}</div>
+                    <p><label>What was your highest score on the math section of the SAT?</label></p>
+                </div>
+                <div className='column right'>
+                    <input 
+                        type='range' 
+                        min={200} 
+                        max={800} 
+                        value={this.state.mathSAT} 
+                        step={10} name='mathSAT' 
+                        onChange={this.handleChange}/>
+                    <span>{this.state.mathSAT}</span>
+                </div>
+            </div>
 
-                        <p><label>What is your high school GPA (normalized to a 4-point scale)?</label></p>
-                        <span><input 
-                                type='range' 
-                                min={0.0} 
-                                max={4.0} 
-                                value={this.state.GPA} 
-                                step={0.01} 
-                                name='GPA' 
-                                onChange={this.handleChange}
-                                />
-                            <Spacer />
-                            <input
-                                type='number'
-                                min={0}
-                                max={4}
-                                step={0.01}
-                                value={this.state.GPA}
-                                maxLength={3}
-                                name='GPA'
-                                size={75}
-                                onChange={this.handleChange} />
-                        <div>{this.state.GPA}</div></span>
-
-                        <p><label>What is your average score across all of the SAT II subject tests that you've taken?</label></p>
-                        <input 
+            <div className='row'>
+                <div className='column left'>
+                    <p><label>What was your highest score on the verbal section of the SAT?</label></p>
+                </div>
+                <div className='column right'>
+                    <input 
+                    type='range' 
+                        min={200} 
+                        max={800} 
+                        value={this.state.verbalSAT} 
+                        step={10} name='verbalSAT' 
+                        onChange={this.handleChange}/>
+                    <span>{this.state.verbalSAT}</span>
+                </div>
+            </div>
+            
+            <div className='row'>
+                <div className='column left'>
+                    <p><label>What is your average score across all of the SAT II subject tests that you've taken?</label></p>
+                </div>
+                <div className='column right'>
+                    <input 
                             type='range' 
                             min={200} 
                             max={800} 
                             value={this.state.subjetSAT} 
                             step={10} name='subjectSAT' 
                             onChange={this.handleChange}/>
-                        <div>{this.state.subjectSAT}</div>
+                    <span>{this.state.subjectSAT}</span>
+                </div>
+            </div>
 
-                        <p><label>Are you a US citizen or permanent resident?</label></p>
-                        Yes<input 
-                            type='checkbox' 
-                            checked={this.state.citizenship} 
-                            name='citizenship' 
-                            onChange={this.check}/>
-                    
-                        <p><label>What is your race/ethnicity?</label></p>
-                        <select 
-                            name="ethnicity" 
-                            checked={this.state.ethnicity} 
-                            onChange={this.handleChange}>
-                                <option value='white'>White</option>
-                                <option value='african-american'>African American</option>
-                                <option value='hispanic'>Hispanic</option>
-                                <option value='asian-american'>Asian-American</option>
-                                <option value='other'>Other</option>
-                                <option value='undisclosed'>Prefer not to say</option>
-                        </select>
-
-                        <p><label>Are you being recruited by Harvard for varsity athletics?</label></p>
-                        Yes<input 
-                            type='checkbox' 
-                            name='athletics'
-                            checked={this.state.athletics}
-                            onChange={this.check}/> 
-
-                        <p><label>Do you have any non-athletic special connection to Harvard? For example, do any of the following cases apply to you:</label></p>
-                        <ul>
-                            <li>You have relatives who are Harvard alumni.</li>
-                            <li>You have relatives who are Harvard employees.</li>
-                            <li>You have relatives who made large donations to Harvard in the past.</li>
-                        </ul>
-                        Yes<input 
-                            type='checkbox' 
-                            name='connections'
-                            checked={this.state.connections}
-                            onChange={this.check}/>
-                    </form>
+            <div className='row'>
+                <div className='column left'>
+                    <p><label>What was your high school GPA (normalized to a 4-point scale)?</label></p>
                 </div>
                 <div className='column right'>
+                    <input 
+                        type='range' 
+                        min={0.0} 
+                        max={4.0} 
+                        value={this.state.GPA} 
+                        step={0.01} 
+                        name='GPA' 
+                        onChange={this.handleChange}
+                        />
+                    {/* <span> | Refine: </span> */}
+                    <input
+                        type='number'
+                        min={0}
+                        max={4}
+                        step={0.01}
+                        value={this.state.GPA}
+                        maxLength={3}
+                        name='GPA'
+                        size={75}
+                        onChange={this.handleChange} />
+                </div>
+            </div>
+
+            <div className='row'>
+                <div className='column left'>
+                    <p><label>Are you a US citizen or permanent resident?</label></p>
+                </div>
+                <div className='column right'>
+                    Yes<input 
+                        type='checkbox' 
+                        checked={this.state.citizenship} 
+                        name='citizenship' 
+                        onChange={this.check}/>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='column left'>
+                    <p><label>What is your race/ethnicity?</label></p>
+                </div>
+                <div className='column right'>
+                <select 
+                        name="ethnicity" 
+                        checked={this.state.ethnicity} 
+                        onChange={this.handleChange}>
+                            <option value='white'>White</option>
+                            <option value='african-american'>African American</option>
+                            <option value='hispanic'>Hispanic</option>
+                            <option value='asian-american'>Asian-American</option>
+                            <option value='other'>Other</option>
+                            <option value='undisclosed'>Prefer not to say</option>
+                    </select>
+                </div>
+            </div>
+
+            <div className='row'>
+                <div className='column left'>
+                    <p><label>Are you being recruited by Harvard for varsity athletics?</label></p>
+                </div>
+                <div className='column right'>
+                    Yes<input 
+                        type='checkbox' 
+                        name='athletics'
+                        checked={this.state.athletics}
+                        onChange={this.check}/> 
+                </div>
+            </div>
+
+            <div className='row'>
+                <div className='column left'>
+                    <p><label>Do any of the following cases apply to you?</label></p>
+                    <ul>
+                        <li>You have relatives who are Harvard alumni.</li>
+                        <li>You have relatives who are Harvard employees.</li>
+                        <li>You have relatives who made large donations to Harvard in the past.</li>
+                    </ul>
+                </div>
+                <div className='column right'>
+                    Yes<input 
+                        type='checkbox' 
+                        name='connections'
+                        checked={this.state.connections}
+                        onChange={this.check}/>
+                </div>
+            </div>
+                        
+                <div>
                     <Probability 
                         mathSAT={this.state.mathSAT}
                         verbalSAT={this.state.verbalSAT}
@@ -174,7 +211,7 @@ class Input extends React.Component {
                         athletics={this.state.athletics}
                         connections={this.state.connections}/>
                 </div>
-            </div>
+        </div>
         );   
     }
 }
